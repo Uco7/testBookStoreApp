adcbd15b6a261888811d0064d2d239ae81766e6a
Uco7 <ucheuc070@gmail.com>
2026-07-14 04:42:56 +0100

    ui funishing2

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -7,6 +7,8 @@ import { AdminUserProvider } from './context/AdminuserContext.jsx'
 import { AdminTimetableProvider } from './context/Admintimetablecontext.jsx'
 import { AdminSubscriptionProvider } from './context/Adminsubscriptioncontext.jsx'
 import { AdminBookProvider } from './context/Adminbookcontex.jsx'
+import PrivacyPolicy from './components/PrivacyPolicy'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -24,5 +26,6 @@ createRoot(document.getElementById('root')).render(
         </AdminTimetableProvider>
       </AdminUserProvider>
     </AdminProvider>
+    <PrivacyPolicy />
   </StrictMode>
 )
